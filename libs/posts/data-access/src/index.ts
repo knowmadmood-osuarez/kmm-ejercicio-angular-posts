414859ea10ba7801c26bb51bf27e88fb18dc0be5
knowmadmood-osuarez <osuarez@knowmadmood.com>
2026-04-09 16:39:17 +0200

    feat(posts): update data-access barrel exports

--- libs/posts/data-access/src/index.ts
+++ libs/posts/data-access/src/index.ts
@@ -1,1 +1,10 @@
-export * from './lib/posts-data-access/posts-data-access';
+// Models
+export type { Post, PaginatedPosts } from './lib/models/post.model';
+export type { Comment } from './lib/models/comment.model';
+
+// Services
+export { PostsService, type PostFilters } from './lib/services/posts.service';
+export { CommentsService } from './lib/services/comments.service';
+
+// Guards
+export { postOwnerGuard } from './lib/guards/post-owner.guard';
