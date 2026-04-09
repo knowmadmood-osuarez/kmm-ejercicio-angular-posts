c84291f02827f77daca6c117dd2523f1d7475cd0
knowmadmood-osuarez <osuarez@knowmadmood.com>
2026-04-09 13:14:04 +0200

    chore(posts): scaffold posts-data-access library - Nx lib with tags type:data-access, scope:posts - Barrel export (index.ts) - Vitest and ESLint configuration

---
+++ libs/posts/data-access/src/lib/posts-data-access/posts-data-access.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-posts-data-access',
+  imports: [],
+  templateUrl: './posts-data-access.html',
+  styleUrl: './posts-data-access.css',
+})
+export class PostsDataAccess {}
