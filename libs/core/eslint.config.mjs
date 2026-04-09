0c5a00423ea427adfe8af1a23baf63e4815b57f3
knowmadmood-osuarez <osuarez@knowmadmood.com>
2026-04-09 13:24:48 +0200

    refactor(config): move Angular app to apps/posts-app - Move app from workspace root to apps/posts-app/ using nx workspace:move - Rename project from kmm-ejercicio-angular-posts to posts-app - Update all build targets, output paths and asset paths - Update scripts in package.json to reference posts-app - Use nx run-many for dev script instead of concurrently - Remove redundant @kmm-ejercicio-angular-posts/* path aliases - Keep only @app/* aliases in tsconfig.base.json - Update nx.json defaultProject to posts-app

--- libs/core/eslint.config.mjs
+++ libs/core/eslint.config.mjs
@@ -2,6 +2,7 @@ import nx from '@nx/eslint-plugin';
 import baseConfig from '../../eslint.base.config.mjs';
 
 export default [
+  ...baseConfig,
   ...nx.configs['flat/angular'],
   ...nx.configs['flat/angular-template'],
   ...baseConfig,
