527e2e273b19b27344a497a54de7611179628a63
knowmadmood-osuarez <osuarez@knowmadmood.com>
2026-04-09 13:41:12 +0200

    chore(tests): add Playwright configuration and initial app test

--- e2e/app.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('app should load', async ({ page }) => {
-  await page.goto('/');
-  await expect(page).toHaveTitle(/kmm-ejercicio-angular-posts/i);
-});
