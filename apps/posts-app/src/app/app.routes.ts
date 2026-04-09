f7efb8abec715c9354329effd79314a7423b3569
knowmadmood-osuarez <osuarez@knowmadmood.com>
2026-04-09 20:31:17 +0200

    feat(auth): enable login route and set default redirect

--- apps/posts-app/src/app/app.routes.ts
+++ apps/posts-app/src/app/app.routes.ts
@@ -1,17 +1,16 @@
 import { Routes } from '@angular/router';
 
 export const appRoutes: Routes = [
+  {
+    path: 'login',
+    loadChildren: () => import('@app/auth/feature-login').then((m) => m.AUTH_ROUTES),
+  },
   // TODO: Uncomment routes as features are implemented
-  // {
-  //   path: 'login',
-  //   loadChildren: () =>
-  //     import('@app/auth/feature-login').then((m) => m.AUTH_ROUTES),
-  // },
   // {
   //   path: 'posts',
   //   canActivate: [authGuard],
   //   children: [ ... ],
   // },
-  { path: '', redirectTo: 'posts', pathMatch: 'full' },
-  { path: '**', redirectTo: 'posts' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'login' },
 ];
