ae94c65a2edf4f5351a059b25939132fa1f51508
knowmadmood-osuarez <osuarez@knowmadmood.com>
2026-04-10 13:43:57 +0200

    feat(app): enable posts route with authGuard and LayoutComponent shell

--- apps/posts-app/src/app/app.routes.ts
+++ apps/posts-app/src/app/app.routes.ts
@@ -1,16 +1,30 @@
 import { Routes } from '@angular/router';
 
+import { authGuard } from '@app/core';
+import { LayoutComponent } from '@app/shared/ui';
+
 export const appRoutes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('@app/auth/feature-login').then((m) => m.AUTH_ROUTES),
   },
-  // TODO: Uncomment routes as features are implemented
-  // {
-  //   path: 'posts',
-  //   canActivate: [authGuard],
-  //   children: [ ... ],
-  // },
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: '**', redirectTo: 'login' },
+  {
+    path: 'posts',
+    canActivate: [authGuard],
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('@app/posts/feature-list').then((m) => m.LIST_ROUTES),
+      },
+      // TODO: feature-form
+      // { path: 'new', loadChildren: () => import('@app/posts/feature-form').then(m => m.FORM_ROUTES) },
+      // TODO: feature-detail
+      // { path: ':id', loadChildren: () => import('@app/posts/feature-detail').then(m => m.DETAIL_ROUTES) },
+      // TODO: feature-form + postOwnerGuard
+      // { path: ':id/edit', loadChildren: () => import('@app/posts/feature-form').then(m => m.FORM_ROUTES) },
+    ],
+  },
+  { path: '', redirectTo: 'posts', pathMatch: 'full' },
+  { path: '**', redirectTo: 'posts' },
 ];
